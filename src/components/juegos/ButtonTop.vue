385a8b5de2ba20e2c04b38e780c1f8fa5e50845e
JAVI-CC <javileg98@hotmail.com>
2021-07-29 16:19:23 +0200

    change color

--- src/components/juegos/ButtonTop.vue
+++ src/components/juegos/ButtonTop.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page-sticky position="bottom-right" :offset="[18, 18]" @click="toTop()">
-    <q-btn padding="xs" fab icon="keyboard_arrow_up" color="primary" glossy />
+    <q-btn padding="xs" fab icon="keyboard_arrow_up" color="blue-10" glossy />
   </q-page-sticky>
 </template>
 
